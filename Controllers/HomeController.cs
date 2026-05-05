94e3d19e6e45da7f81b2060c049986db923da923
CATALINA SOL MILSZTAIN <50600085@ort>
2026-05-05 08:47:32 -0300

    inicio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EJ4_FORM.Models;
+
+namespace EJ4_FORM.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public ActionResult GuardarDatos(string nombre, int edad, int DNI, string trabaja, string tipoEmpleo, double ingresos, 
+    string deudas, List<string> deudi, double monto, string plazo, string terminosyco){
+        
+        return 
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EJ4_FORM.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
